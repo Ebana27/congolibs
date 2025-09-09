606a1b51ac4c80ddc259a5eef2f9cc185d00ef85
Ebana27 <141494422+Ebana27@users.noreply.github.com>
2025-09-09 22:21:44 +0100

    add root files

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'tailwindcss/tailwind.css';
+
+// Vous pouvez ajouter ici votre logique JavaScript pour l'application
+// Exemple : 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Congolibs Project avec Tailwind CSS est prêt !');
+});
